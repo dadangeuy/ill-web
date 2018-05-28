db4dec687f81cc2b2a5ace1afc86f61d32b3560d
Irsyad Rizaldi <irsyad.rizaldi97@gmail.com>
2018-05-28 07:21:39 +0700

    pinjam page

--- src/app/peminjam/pinjam/pinjam.component.ts
+++ src/app/peminjam/pinjam/pinjam.component.ts
@@ -1,4 +1,8 @@
 import {Component, OnInit} from '@angular/core';
+import {Observable} from 'rxjs';
+import {Book} from '../../model/Book';
+import {BookService} from '../../service/book.service';
+import {ActivatedRoute} from '@angular/router';
 
 @Component({
   selector: 'app-pinjam',
@@ -6,11 +10,15 @@ import {Component, OnInit} from '@angular/core';
   styleUrls: ['./pinjam.component.scss']
 })
 export class PinjamComponent implements OnInit {
+  book$: Observable<Book> = this.service.getBook(Number(this.route.snapshot.params['id']));
 
-  constructor() {
+  constructor(private service: BookService, private route: ActivatedRoute) {
   }
 
   ngOnInit() {
   }
 
+  attemptPinjam() {
+    alert('Peminjaman Berhasil Diajukan!\nMohon Menunggu Untuk Persetujuan.');
+  }
 }
