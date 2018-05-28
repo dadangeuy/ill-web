69ef30fdb8d58bb7c5185dc6b652af4ccd4979ca
Irsyad Rizaldi <irsyad.rizaldi97@gmail.com>
2018-05-28 10:07:11 +0700

    fix logout header

---
+++ src/app/pustakawan/konfirmasi/konfirmasi.component.ts
@@ -0,0 +1,16 @@
+import {Component, OnInit} from '@angular/core';
+
+@Component({
+  selector: 'app-konfirmasi',
+  templateUrl: './konfirmasi.component.html',
+  styleUrls: ['./konfirmasi.component.scss']
+})
+export class KonfirmasiComponent implements OnInit {
+
+  constructor() {
+  }
+
+  ngOnInit() {
+  }
+
+}
---
+++ src/app/pustakawan/riwayat/riwayat.component.ts
@@ -0,0 +1,16 @@
+import {Component, OnInit} from '@angular/core';
+
+@Component({
+  selector: 'app-riwayat',
+  templateUrl: './riwayat.component.html',
+  styleUrls: ['./riwayat.component.scss']
+})
+export class RiwayatComponent implements OnInit {
+
+  constructor() {
+  }
+
+  ngOnInit() {
+  }
+
+}
