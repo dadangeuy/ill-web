27c82998e6f172bc33a115f99f32f3f7d8575608
Irsyad Rizaldi <irsyad.rizaldi97@gmail.com>
2018-05-31 23:50:31 +0700

    add ubah content

--- src/app/pustakawan/koleksi/ubah/ubah.component.ts
+++ src/app/pustakawan/koleksi/ubah/ubah.component.ts
@@ -1,4 +1,8 @@
 import {Component, OnInit} from '@angular/core';
+import {ActivatedRoute} from '@angular/router';
+import {BookService} from '../../../service/book.service';
+import {Observable, of} from 'rxjs';
+import {Book} from '../../../model/Book';
 
 @Component({
   selector: 'app-ubah',
@@ -6,8 +10,10 @@ import {Component, OnInit} from '@angular/core';
   styleUrls: ['./ubah.component.scss']
 })
 export class UbahComponent implements OnInit {
+  book$: Observable<Book> = this.service.getBook(Number(this.route.snapshot.params['id']));
+  statuses$: Observable<String[]> = of(['AVAILABLE', 'UNAVAILABLE']);
 
-  constructor() {
+  constructor(private route: ActivatedRoute, private service: BookService) {
   }
 
   ngOnInit() {
